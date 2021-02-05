cc8c7bfff00bcca08fcf27403da0264b58f6fe07
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-02-05 15:24:46 +0700

    read

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SavedInstanceStateNoVM
- 
+# SavedInstanceStateExample
+
+Example code
+[MainActivity](https://github.com/gzeinnumer/SavedInstanceStateNoVM/blob/master/app/src/main/java/com/gzeinnumer/savedinstancestatenovm/MainActivity.java)
+
+source https://stackoverflow.com/questions/151777/how-to-save-an-activity-state-using-save-instance-state
+---
+
+https://stackoverflow.com/questions/2533517/how-to-test-code-built-to-save-restore-lifecycle-of-an-activity
+
+![](https://github.com/gzeinnumer/SavedInstanceStateExample/blob/master/preview/example11.jpeg)
+
+---
+
+```
+Copyright 2021 M. Fadli Zein
+```
--- app/src/main/java/com/gzeinnumer/savedinstancestatenovm/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/savedinstancestatenovm/MainActivity.java
@@ -1,8 +1,9 @@
 package com.gzeinnumer.savedinstancestatenovm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putBoolean("MyBoolean", true);
+        savedInstanceState.putDouble("myDouble", 1.9);
+        savedInstanceState.putInt("MyInt", 1);
+        savedInstanceState.putString("MyString", "Welcome back to Android");
+        // etc.
+
+        Toast.makeText(this, "savedInstanceState saved", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        boolean myBoolean = savedInstanceState.getBoolean("MyBoolean");
+        double myDouble = savedInstanceState.getDouble("myDouble");
+        int myInt = savedInstanceState.getInt("MyInt");
+        String myString = savedInstanceState.getString("MyString");
+        Toast.makeText(this, "savedInstanceState loaded: " + myBoolean + "_" + myDouble + "_" + myInt + "_" + myString, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //on fragment
+//    @Override
+//    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+//        super.onViewStateRestored(savedInstanceState);
+//        // Restore UI state from the savedInstanceState.
+//        // This bundle has also been passed to onCreate.
+//        boolean myBoolean = savedInstanceState.getBoolean("MyBoolean");
+//        double myDouble = savedInstanceState.getDouble("myDouble");
+//        int myInt = savedInstanceState.getInt("MyInt");
+//        String myString = savedInstanceState.getString("MyString");
+//    }
 }
